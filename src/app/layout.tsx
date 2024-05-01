69660fe5da51ea783f881fd6178f911dcc054d9b
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2024-05-01 20:24:39 -0300

    Componente Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html className={inter.variable} lang="pt-BR">
-      <body>{children}</body>
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   )
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,46 @@
+import { Search, ShoppingBag } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Header() {
-  return <header>Header</header>
+  return (
+    <header className="flex item-center justify-between">
+      <section className="flex items-center gap-5">
+        <Link href="/" className="text-2xl font-extrabold text-white">
+          devstore
+        </Link>
+
+        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
+          <Search className="w-5 h-5 text-zinc-500" />
+
+          <input
+            type="text"
+            placeholder="Buscar produtos..."
+            className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
+          />
+        </form>
+      </section>
+
+      <section className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
+          <ShoppingBag className="h-4 w-4" />
+
+          <span className="text-sm">Cart (0)</span>
+        </div>
+
+        <div className="w-px h-4 bg-zinc-700" />
+
+        <Link href="/" className="flex items-center gap-2 hover:underline">
+          <span className="text-sm">Account</span>
+          <Image
+            src="https://github.com/jerp86.png"
+            alt="Imagem de perfil do usuário"
+            className="h-6 w-6 rounded-full"
+            width={24}
+            height={24}
+          />
+        </Link>
+      </section>
+    </header>
+  )
 }
