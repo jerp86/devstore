bea424097c7ddb98433cf015995317f04e4dc719
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2024-05-04 12:09:45 -0300

    Fetch de dados no Produto

--- src/app/(store)/products/[slug]/page.tsx
+++ src/app/(store)/products/[slug]/page.tsx
@@ -1,12 +1,36 @@
+import { api } from '@/data/api'
+import { Product } from '@/data/types/products'
+import { currencyFormat } from '@/utils/currencyFormat'
 import Image from 'next/image'
 
-export default function ProductPage() {
+export interface ProductProps {
+  params: {
+    slug: string
+  }
+}
+
+async function getProductBySlug(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  })
+
+  const product = await response.json()
+  return product
+}
+
+export default async function ProductPage({ params }: ProductProps) {
+  const product = await getProductBySlug(params.slug)
+  const { description, image, price, title } = product
+
   return (
     <section className="relative grid max-h-[860px] grid-cols-3">
       <aside className="col-span-2 overflow-hidden">
         <Image
-          src="/moletom-never-stop-learning.png"
-          alt=""
+          src={image}
+          alt={description}
+          title={title}
           width={1000}
           height={1000}
           quality={100}
@@ -14,21 +38,17 @@ export default function ProductPage() {
       </aside>
 
       <aside className="flex flex-col justify-center px-12">
-        <h1 className="text-3xl font-bold leading-tight">
-          Moletom Never Stop Learning
-        </h1>
+        <h1 className="text-3xl font-bold leading-tight">{title}</h1>
 
-        <p className="mt-2 leading-relaxed text-zinc-400">
-          Moletom fabricado com 88% de algodão e 12% de poliéster.
-        </p>
+        <p className="mt-2 leading-relaxed text-zinc-400">{description}</p>
 
         <div className="mt-8 flex items-center gap-3">
           <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-            R$129
+            {currencyFormat(price)}
           </span>
 
           <span className="text-sm text-zinc-400">
-            Em 12x s/ juros de R110,75
+            Em 12x s/ juros de {currencyFormat(price / 12)}
           </span>
         </div>
 
---
+++ src/utils/currencyFormat.ts
@@ -0,0 +1,8 @@
+export const currencyFormat = (value: number) => {
+  return value.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+  })
+}
