8d0fde8de8c5578beb46e8a5bc83d6240ab4622d
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2024-05-04 19:59:36 -0300

    substituído domais por remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['github.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 }
 
