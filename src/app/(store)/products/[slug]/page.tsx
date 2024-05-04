a114acbd79f6c7f6257a39d9752ac97402ead146
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2024-05-04 18:14:34 -0300

    Geração estática na build

--- src/app/(store)/products/[slug]/page.tsx
+++ src/app/(store)/products/[slug]/page.tsx
@@ -29,6 +29,13 @@ export async function generateMetadata({
   return { title }
 }
 
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+  const products: Product[] = await response.json()
+
+  return products.map(({ slug }) => ({ slug }))
+}
+
 export default async function ProductPage({ params }: ProductProps) {
   const product = await getProductBySlug(params.slug)
   const { description, image, price, title } = product
